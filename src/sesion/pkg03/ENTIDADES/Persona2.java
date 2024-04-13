4b8c5762087e6d2fdb83ba884a2b03cddecb3f9c
SaraSantisteban12 <166246482+SaraSantisteban12@users.noreply.github.com>
2024-04-13 13:41:32 -0500

    Add files via upload

---
+++ src/sesion/pkg03/ENTIDADES/Persona2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion.pkg03.ENTIDADES;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Persona2 {
+    
+}
